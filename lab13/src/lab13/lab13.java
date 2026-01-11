d0073bad95649ae64825e53269f06086cd82ccca
18-july-2005 <154409778+18-july-2005@users.noreply.github.com>
2026-01-11 18:42:13 +0500

    added lab 13

---
+++ lab13/src/lab13/lab13.java
@@ -0,0 +1,31 @@
+package lab13;
+
+class Calculator {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int subtract(int a, int b) {
+        return a - b;
+    }
+
+    int multiply(int a, int b) {
+        return a * b;
+    }
+}
+
+public class lab13 {
+
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Addition: " + calc.add(a, b));
+        System.out.println("Subtraction: " + calc.subtract(a, b));
+        System.out.println("Multiplication: " + calc.multiply(a, b));
+    }
+}
